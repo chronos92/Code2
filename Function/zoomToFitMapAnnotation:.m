4a9071d8c183e0a65d8ce13325addac616b07390
chronos92 <boris.falcinelli@gmail.com>
2014-10-23 11:07:28 +0200

    Update zoomToFitMapAnnotation:.m

--- Function/zoomToFitMapAnnotation:.m
+++ Function/zoomToFitMapAnnotation:.m
@@ -11,26 +11,24 @@
     bottomRightCoord.latitude = 90;
     bottomRightCoord.longitude = -180;
     
-    for(id<MKAnnotation> annotation1 in self.mappaView.annotations){
-        if ([annotation1 isMemberOfClass:[poiAnnotation class]]){
-            poiAnnotation * annotation = annotation1;
+    for(id<MKAnnotation> annotation in self.mappaView.annotations){
             atLeastOne = true;
             topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
             topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
             bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
             bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
-        }else{
-            MKUserLocation* annotation = annotation1;
-            atLeastOne = true;
-            topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
-            topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
-            bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
-            bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
-        }
     }
     if (!atLeastOne) {
         return;
     }
+
+    if ([CLLocationManager locationServicesEnabled]) {
+        topLeftCoord.longitude = fmin(topLeftCoord.longitude, [[MyLocation sharedMyLocation] getUserLocation].longitude);
+        topLeftCoord.latitude = fmax(topLeftCoord.latitude, [[MyLocation sharedMyLocation] getUserLocation].latitude);
+        bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, [[MyLocation sharedMyLocation] getUserLocation].longitude);
+        bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, [[MyLocation sharedMyLocation] getUserLocation].latitude);
+    }
+
     MKCoordinateRegion region;
     region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
     region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
@@ -38,4 +36,3 @@
     region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.2; // Add a little extra space on the sides
     [self.mappaView setRegion:[self.mappaView regionThatFits:region] animated:animated];
 }
-
